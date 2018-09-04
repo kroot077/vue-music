73c470ec61baa8307eedcb3dac9db6a176baf80a
Keith Root <hazelroot11@gmail.com>
2018-09-03 19:50:17 -0600

    Making things work and testing

--- src/store.js
+++ src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
+
+Vue.use(Vuex)
+
+let musicApi = Axios.create({
+  baseURL: "https://itunes.apple.com/search?term=",
+  timeout: 3000
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    songs: []
   },
   mutations: {
-
+    setSongs(state, data) {
+      state.songs = data
+    } 
   },
   actions: {
-
+    search({commit, dispatch}, searchTerm) {
+      musicApi.get(searchTerm)
+        .then(res => {
+          commit('setSongs', res.data.results)
+        })
+    },
+    //addToPlaylist
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="search">
+        <input type="text" v-model="songList" placeholder="Song Title">
+        <button type="submit">Search</button>
+    </form>
+    <div class="">
+      <Song></Song>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Song from '@/components/Songs.vue'
 
 export default {
   name: 'home',
+  data(){
+    return{
+      songList: {}
+    }
+  },
+  computed: {
+    songs() {
+      return this.$store.state.songs
+    }
+  },
+  methods: {
+    search() {
+      this.$store.dispatch("search", this.songList)
+    }
+  },
   components: {
-    HelloWorld
+    Song
   }
 }
 </script>
